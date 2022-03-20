a9a49d7f5095ba62543362db31e4efd5ba76bf6c
stonarini <stonarini@cifpfbmoll.eu>
2022-03-20 11:56:07 +0100

    chore(src): add main method and tests (with AssertJ dependency) provided by @dfleta

---
+++ src/main/java/edu/poniperro/ArnoldMain.java
@@ -0,0 +1,26 @@
+package edu.poniperro;
+
+import edu.poniperro.logica.Planeta;
+
+public class ArnoldMain {
+
+    public static void main(String[] args) {
+
+        double peso = 1.0; // kg
+
+        for (Planeta planeta : Planeta.values()) {
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+
+        System.out.println("\nYour weight only on the terrestrial planets: ");
+        for (Planeta planeta : Planeta.getPlanetasTerrestres()) {
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+
+        System.out.println("\nYour weight only on the gas giant planets: ");
+        for (Planeta planeta : Planeta.getGigantesGaseosos()) {
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+    }
+
+}
---
+++ src/main/java/edu/poniperro/logica/Planeta.java
@@ -0,0 +1,5 @@
+package edu.poniperro.logica;
+
+public enum Planeta {
+
+}
